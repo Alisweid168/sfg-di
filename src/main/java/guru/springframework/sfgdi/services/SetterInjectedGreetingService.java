7dc288e10be91a8570690c41bfbd2fb957016a71
Alisweid168 <ali.sweidan003@gmail.com>
2024-03-04 21:37:19 +0200

    updatingsmallthing

--- src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class SetterInjectedGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello world! - Property";
+        return "Hello world! - Setter";
     }
 }
